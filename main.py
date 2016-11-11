4c77bd9a33cf99bff8af680ae0628b7351085f09
datalater <the7mincheol@gmail.com>
2016-11-11 15:26:56 +0900

    first commit

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 # 도서관 좌석 현황 url
 # http://library.hufs.ac.kr:8091/mobile_new/lib_studyroom_state.asp
+#
 # 도서관 좌석 연장 url
 # http://library.hufs.ac.kr:8091/mobile_new/lib_studyroom_charge.asp
+#
+# 도서관 login url
+# https://mlibrary.hufs.ac.kr/identity/Login.ax?url=/guide/Static.ax?page=STUDY_ROOMS&lib=01
